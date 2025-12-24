0e147039577d24a1c20b89acdc34a4f604427847
hyeonjun <stuhk56@gmail.com>
2025-12-24 18:03:30 +0900

    feat/dashboard 수정

--- src/modules/images/image-route.ts
+++ src/modules/images/image-route.ts
@@ -1,13 +1,1 @@
-import { Router } from 'express';
-import { imageController } from './images/upload';
-import { upload } from '../../middlewares/upload-middleware.js';
 
-const imageRouter = Router();
-
-imageRouter.post(
-  '/upload',
-  upload.single('image'), // 'image' is the field name for the uploaded file
-  imageController.uploadImage
-);
-
-export { imageRouter };
