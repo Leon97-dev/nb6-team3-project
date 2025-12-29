1095eb4b6fea096c726cde11f27e6870b46e7b67
hyeonjun <stuhk56@gmail.com>
2025-12-29 09:53:31 +0900

    업로드 스키마

---
+++ src/modules/uploads/upload-controller.ts
@@ -0,0 +1,25 @@
+/**
+ * @description 업로드 컨트롤러 모듈
+ * 파일 업로드를 처리하는 기능을 제공합니다.
+ * @author 이호성
+ * @date 2025-12-29
+ * @version 1.0
+ **/
+
+import type { Request, Response } from 'express';
+
+export const uploadController = {
+  // 파일 업로드 처리 메서드
+  uploadFile: (req: Request, res: Response) => {
+    // 업로드된 파일 정보는 req.file 또는 req.files에 저장됩니다.
+    if (!req.file) {
+      return res.status(400).json({ message: '파일이 업로드되지 않았습니다.' });
+    }
+
+    // 업로드된 파일 정보 반환
+    res.status(200).json({
+      message: '파일 업로드 성공',
+      fileInfo: req.file,
+    });
+  },
+};
