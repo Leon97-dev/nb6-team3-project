ae32ce8c56dd54c6700416c400a1bd437a060f9c
hyeonjun <stuhk56@gmail.com>
2026-01-12 13:41:44 +0900

    대시보드 최종본

--- src/modules/dashboards/dashboard-route.ts
+++ src/modules/dashboards/dashboard-route.ts
@@ -2,11 +2,11 @@
  * @description 대시보드 라우터 모듈
  * 대시보드 관련 요청을 처리하는 기능을 제공합니다.
  * @author 정현준
- * @date 2026-01-09
+ * @date 2026-01-12
  * @version 1.0
  **/
 import { Router } from 'express';
-import { dashboardController } from './customers/customer-controller.js';
+import { dashboardController } from './dashboard-controller.js';
 import { requireAuth } from '../../middlewares/auth.js';
 import asyncHandler from '../../errors/async-handler.js';
 
