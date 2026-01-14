239af4fe021b784d78328d782bf6cbfe8bc5c4b9
Leon <gomdole32@naver.com>
2026-01-14 13:23:59 +0900

    fix: 프론트 베포 에러로 인한 eslint 끄기

--- public/project-dearcarmate-fe-main/next.config.js
+++ public/project-dearcarmate-fe-main/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
     prependData: `@use "src/styles/utils.scss" as *;`,
   },
   images: {
@@ -15,3 +15,12 @@ module.exports = {
     ],
   },
 };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
