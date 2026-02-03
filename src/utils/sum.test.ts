22eac94308f1e10dd995bc678f041631df5fc55d
Leon <gomdole32@naver.com>
2026-02-03 22:43:47 +0900

    test: add first jest test

---
+++ src/utils/sum.test.ts
@@ -0,0 +1,8 @@
+import { sum } from './sum.js';
+import { describe, expect, test } from '@jest/globals';
+
+describe('sum', () => {
+  test('adds two numbers', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
